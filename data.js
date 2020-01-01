219255efee29eff166349fab73bcd9bb1b7181a9
Steve Bennett <stevage@gmail.com>
2020-01-01 11:09:10 +1100

    Refactor

--- data.js
+++ data.js
@@ -3,25 +3,10 @@ const hash = require("number-generator/lib/murmurhash3_x86_32");
 const rng = require("number-generator/lib/aleaRNGFactory")();
 const fakeName = require('fake-town-name');
 
-const cantor = (x, y) => 0.5 * (x + y) * (x + y + 1) + y;
 const seeds = {}
-function seedify(x,y) {
-    const s = ((x % (2 << 15)) << 16) + (y % (2 << 15))
-    if (s === 0) { 
-        console.log('0!', x, y);
-    }
-    if (seeds[s]) {
-        // console.warn('Repeated seed!', seeds[s], [x,y]);
-    }
-    seeds[s] = [x,y];
-    return s;
-}
 
 function hashSeed(x, y) {
-    // return hash(String(seedify(Math.round(x), Math.round(y))));
-    console.log(`${x}${y}`);
     return hash(`${x}${y}`);
-
 }
 
 function setSeed(seed) {
@@ -40,29 +25,18 @@ function pointsForBounds([minx, miny, maxx, maxy]) {
     for (let x = q(minx); x <= q(maxx); x += 1/ scale) {
         for (let y = q(miny); y <= q(maxy); y += 1/ scale) {
             if (x < minx || y < miny || x >= maxx || y >= maxy) {
-                // console.log('Out of bounds',x,y);
                 continue;
             }
-            // console.log('Ok',x,y);
-
             points.push([x,y]);
         }
     };
     return points;
 }
 
-function makePoint(coordinates, seed) {
-    const hex = () => '0123456789abcdef'[Math.floor(random()*16)];
+function makePoint(coordinates, properties) {
     return {
         type: 'Feature',
-        properties: {
-            // 'marker-color': `hsl(${random() * 360},${50 + random() * 50}%,${random() * 30 + 30}%)`,
-            'marker-color': `#${hex()}${hex()}${hex()}`,
-            'marker-symbol': 'circle',
-            'marker-size': 'medium',
-            name: fakeName({seed}),
-            size: Math.ceil(random()*random()*random()*5)
-        },
+        properties,
         geometry: {
             type: 'Point',
             coordinates
@@ -70,22 +44,35 @@ function makePoint(coordinates, seed) {
     }
 }
 
-module.exports = function dataForBounds([minx, miny, maxx, maxy]) {
-    function wrap(x, min, max) {
+function town(x, y, [minx, miny, maxx, maxy]) {
+    // unsolved problem: by constraining the x/y to the bounding box for a tile, the generated data becomes scale dependent
+    // so as we zoom in/out, points near the edges of tiles jump around
+    function wrap(z, min, max) {
         const range = max - min;
-        return (x - min) % range + min;
+        return (z - min) % range + min;
     }
+
+
+    const seed = hashSeed(Math.round(x * scale), Math.round(y * scale));
+    setSeed(seed);
+    const props = {
+        name: fakeName({seed}),
+        size: Math.ceil(random()*random()*random()*5)
+    };
+    const coords = [
+        wrap(x + random() /scale, minx, maxx), 
+        wrap(y + random() /scale, miny, maxy), 
+    ];
+    return makePoint(coords, props)
+
+}
+
+module.exports = function dataForBounds([minx, miny, maxx, maxy]) {
 
     const features = [];
     pointsForBounds([minx, miny, maxx, maxy]).forEach(([x,y]) => {
-        const seed = hashSeed(Math.round(x * scale), Math.round(y * scale))
-        setSeed(seed);
-
-        const coords = [
-            wrap(x + random() /scale, minx, maxx), 
-            wrap(y + random() /scale, miny, maxy), 
-        ];
-        features.push(makePoint(coords, seed))
+        
+        features.push(town(x, y, [minx, miny, maxx, maxy]))
     });
     console.log(`${features.length} points.`);
     return {
